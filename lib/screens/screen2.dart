52ee9f57aa83d958e44ea404d02ff623a16ae208
Ssenoga Herman <hermanssenoga@gmail.com>
2025-11-12 17:19:35 +0300

    Created a form to create a new task in screen2

--- lib/screens/screen2.dart
+++ lib/screens/screen2.dart
@@ -1,17 +1,90 @@
 import 'package:flutter/material.dart';
 
-class Screen2 extends StatelessWidget {
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:go_router/go_router.dart';
+
+class Screen2 extends StatefulWidget {
   const Screen2({super.key});
 
   @override
+  State<Screen2> createState() => _Screen2State();
+}
+
+class _Screen2State extends State<Screen2> {
+  final _formKey = GlobalKey<FormBuilderState>();
+  List<dynamic> items=['low','average','high'];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      
-      
-      body: Center(
-        child: Text("Second Screen"),
+      appBar: AppBar(
+        title: Center(
+          child:Text("Customize your tasks")
+        ),
       ),
+      body: Center(
+        child:FormBuilder(
+          key: _formKey,
+          child:  Column(
+            children: [
+              Text("Create New Task",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.blue
+                ),
+
+              ),
+
+              SizedBox(width: 500,
+                child: FormBuilderTextField(
+                  name: 'title',
+                  decoration: InputDecoration(
+                    labelText: "Task title",
+                    border: OutlineInputBorder()
+                  ),
+                ),
+              ),
+              SizedBox(height:20),
+              SizedBox(
+                width: 500,
+                child: FormBuilderDropdown(name: 'Priority',
+                    decoration: InputDecoration(
+                        labelText: "Priority",
+                        border: OutlineInputBorder()
+                    ),
+                    items:
+                [DropdownMenuItem(value: 'High', child: Text('High')),
+                DropdownMenuItem(value: 'Medium', child: Text('Medium')),
+                DropdownMenuItem(value: 'Low', child: Text('Low')),
+                            ]
+                ),
+              ),
+              SizedBox(height: 20,),
+
+              SizedBox(width: 500,
+                child: FormBuilderTextField(
+                  name: 'description',
+                  maxLines: 3,
+                  decoration: InputDecoration(
+                      labelText: "Description",
+                      border: OutlineInputBorder()
+                  ),
+                ),
+              )
+              ,SizedBox(height: 20,),
+              ElevatedButton(onPressed: (){ 
+                
+              }, child: Text('ADD TASK'))
+            ],
+          ),
+        )
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        context.goNamed('home');
+
+      },
+      child: Icon(Icons.arrow_back),),
     );
   }
 }
